70407b261461aa5c8cc2e0a4064bfed379d05f15
OskarLyresten <oskarlyresten@gmail.com>
2022-10-13 12:39:22 +0200

    Added timing and plotting

--- MA4/MA4_Files/MA4_2.py
+++ MA4/MA4_Files/MA4_2.py
@@ -2,12 +2,45 @@
 
 from person import Person
 from numba import njit
+from time import perf_counter as pc
+from matplotlib import pyplot as plt
+
+
+def fib_py(n):
+    if n <= 0:
+        return 1
+    else:
+        return fib_py(n-1) + fib_py(n-2)
+    
+@njit
+def fib_numba(n):
+    if n <= 0:
+        return 1
+    else:
+        return fib_numba(n-1) + fib_numba(n-2)
+
 
 def main():
-	f = Person(5)
-	print(f.fib())
-	f.set(7)
-	print(f.fib())
+	
+	py_times = []
+	numba_times = []
+	
+	N = [5, 10, 15, 20, 25, 30]
+	for n in N:
+		start = pc()
+		fib_py(n)
+		py_times.append(pc()-start())
+  
+		start = pc()
+		fib_numba(n)
+		numba_times.append(pc()-start())
+  
+	plt.plot(N, py_times)
+	plt.plot(N, numba_times)
+ 
+	plt.legend("Python", "Numba")
+ 
+	plt.savefig("plot.png")
 
 if __name__ == '__main__':
 	main()
